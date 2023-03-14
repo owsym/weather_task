0614bd9513a6eb4cbcb5e8e2d24fa6488dcd2185
Usama Farooq <ocamafarooq.97@gmail.com>
2023-03-14 18:27:22 +0500

    t2 files update

---
+++ Task/t2.py
@@ -0,0 +1,24 @@
+import os
+
+from Utils.read_files import read_files
+
+
+file_values = read_files()
+
+Temperatures = {}
+for file_value in file_values:
+    if file_value and file_value[1]:
+        Temperatures[file_value[0]] = float(file_value[1])
+
+dates_with_difference_of_7 = set()
+for date1, temp1 in Temperatures.items():
+    for date2, temp2 in Temperatures.items():
+        if date1 < date2 and abs(temp1 - temp2) == 7:
+            dates_with_difference_of_7.add((date1, date2))
+
+if dates_with_difference_of_7:
+    print("Dates with a difference of exactly 7 between Min and Max TemperatureC:")
+    for date1, date2 in dates_with_difference_of_7:
+        print(date1, "and", date2)
+else:
+    print("No dates found with a difference of exactly 7 between Min and Max TemperatureC.")
