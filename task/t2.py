af57c023b3998f96f5103e9896264df3105c14f7
Usama Farooq <ocamafarooq.97@gmail.com>
2023-03-15 21:17:54 +0500

    changing some files

--- task/t2.py
+++ task/t2.py
@@ -2,23 +2,24 @@ from utils.read_files import read_files_t2
 
 file_values = read_files_t2()
 
-temperatures = {}
+temperatures = []
 for file_value in file_values:
     if file_value and len(file_value) >= 4 and file_value[1] and file_value[3]:
-        min_temp = float(file_value[3])
-        max_temp = float(file_value[1])
-        avg_temp = (min_temp + max_temp) / 2
-        temperatures[file_value[0]] = avg_temp
+        date = file_value[0]
+        min_temp = int(file_value[3])
+        max_temp = int(file_value[1])
+        temperatures.append({'date': date, 'min_temp': min_temp, 'max_temp': max_temp})
 
-dates_with_difference_of_7 = set()
-for date1, temp1 in temperatures.items():
-    for date2, temp2 in temperatures.items():
-        if date1 < date2 and abs(temp1 - temp2) == 7:
-            dates_with_difference_of_7.add((date1, date2))
+# Filter dates with a difference of exactly 7 between min and max temp
+filtered_temperatures = []
+for temp in temperatures:
+    if abs(temp['max_temp'] - temp['min_temp']) == 7:
+        filtered_temperatures.append(temp)
 
-if dates_with_difference_of_7:
+# Print filtered dates in desired format
+if filtered_temperatures:
     print("Dates with a difference of exactly 7 between Min and Max TemperatureC:")
-    for date1, date2 in dates_with_difference_of_7:
-        print(date1, "and", date2)
+    for temp in filtered_temperatures:
+        print(f"Date: {temp['date']}, Max TemperatureC: {temp['max_temp']}, Min TemperatureC: {temp['min_temp']}")
 else:
     print("No dates found with a difference of exactly 7 between Min and Max TemperatureC.")
--- utils/read_files.py
+++ utils/read_files.py
@@ -1,11 +1,25 @@
 import os
 
+#All files content reading function
 def get_contents_of_one_file(file_content):
     file_values = []
     for row in file_content.split("\n")[1:-1]:
         file_values.append(row.split(","))
-
     return file_values
+
+#Content reading function for t1
+def read_files_t1():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_names = os.listdir(os.path.join(dir_path, '..', 'weatherfiles'))
+    file_values = []
+
+    for file_name in file_names:
+        with open(os.path.join(dir_path, '..', 'weatherfiles', file_name), "r") as file_reader:
+            file_content = file_reader.read()
+            file_values += get_contents_of_one_file(file_content)
+    return file_values
+
+#Content reading function for t2
 def read_files_t2():
     dir_path = os.path.dirname( os.path.realpath( __file__ ) )
     file_names = os.listdir( os.path.join( dir_path, '..', 'weatherfiles' ) )
@@ -15,20 +29,4 @@ def read_files_t2():
         with open( os.path.join( dir_path, '..', 'weatherfiles', file_name ), "r" ) as file_reader:
             file_content = file_reader.read()
             file_values += get_contents_of_one_file(file_content)
-    return file_values
-
-
-
-def read_files_t1():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_names = os.listdir(os.path.join(dir_path, '..', 'weatherfiles'))
-
-    file_values = []
-
-    for file_name in file_names:
-        with open(os.path.join(dir_path, '..', 'weatherfiles', file_name), "r") as file_reader:
-            file_content = file_reader.read()
-
-            file_values += get_contents_of_one_file(file_content)
-
     return file_values
